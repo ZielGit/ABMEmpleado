23b2a00ff316d40a535bff0f8a4cc896bd80904c
Frans <frans_421_12@hotmail.com>
2024-02-23 23:03:36 -0500

    feat: Servicio y modelo empleado

---
+++ src/app/services/empleado.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Empleado } from '../models/empleado';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmpleadoService {
+  listEmpleado: Empleado[] = [
+    {
+      nombreCompleto: 'Lucas Martinez', correo: 'lmartinez@gmail.com', telefono: 357123489,
+      sexo: 'Masculino', fechaIngreso: new Date(), estadoCivil: 'soltero'
+    },
+    {
+      nombreCompleto: 'Rodrigo Aliaga', correo: 'raliaga@gmail.com', telefono: 658123489,
+      sexo: 'Masculino', fechaIngreso: new Date('2019-04-27'), estadoCivil: 'soltero'
+    }
+  ];
+
+  constructor() { }
+
+  getEmpleados() {
+    return this.listEmpleado.slice();
+  }
+}
